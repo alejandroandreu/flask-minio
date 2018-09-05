ff21dd42bfaab8bf1eec0df9bdc1697acc79eb0f
Alejandro Andreu <alejandro@midokura.com>
2018-09-05 15:19:27 +0200

    Add long description to package

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ Adds Minio support to your Flask application
 from setuptools import setup
 
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='Flask-Minio',
     version='0.1',
@@ -15,7 +18,8 @@ setup(
     author='Alejandro Andreu',
     author_email='contact@alejandroandr.eu',
     description='Adds Minio support to your Flask application',
-    long_description=__doc__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     py_modules=['flask_minio'],
     zip_safe=False,
     platforms='any',
@@ -28,7 +32,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
